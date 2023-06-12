bf1d7221cb6b515c1147760a3aedcd41f167ecc1
d.karyakin <3754479555552@gmail.com>
2023-06-12 17:35:08 +0300

    init

---
+++ Recieve/Recieve/Program.cs
@@ -0,0 +1,193 @@
+﻿#region a work queue is that each task is delivered to exactly one worker
+
+/*
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+//https://www.rabbitmq.com/tutorials/tutorial-two-dotnet.html
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "task_queue",
+    durable: true,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+Console.WriteLine(" [*] Waiting for messages.");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    byte[] body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received {message}");
+
+    int dots = message.Split('.').Length - 1;
+    Thread.Sleep(dots * 1000);
+
+    Console.WriteLine(" [x] Done");
+
+    // here channel could also be accessed as ((EventingBasicConsumer)sender).Model
+    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+};
+channel.BasicConsume(queue: "task_queue",
+    autoAck: false,
+    consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
+*/
+
+#endregion
+
+#region exchange to Direct
+
+/*
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "logs_direct", type: ExchangeType.Direct);
+
+// declare a server-named queue
+var queueName = channel.QueueDeclare().QueueName;
+channel.QueueBind(queue: queueName,
+    exchange: "logs_direct",
+    routingKey: string.Empty);
+
+Console.WriteLine(" [*] Waiting for logs.");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    byte[] body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] {message}");
+};
+channel.BasicConsume(queue: queueName,
+    autoAck: true,
+    consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();*/
+
+#endregion
+
+#region MyRegion
+
+/*
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "topic_logs", type: ExchangeType.Topic);
+
+// declare a server-named queue
+var queueName = channel.QueueDeclare().QueueName;
+channel.QueueBind(
+    queue: queueName,
+    exchange: "topic_logs",
+    routingKey: "ROUT_KYE");
+
+Console.WriteLine(" [*] Waiting for logs.");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    var routingKey = ea.RoutingKey;
+    Console.WriteLine($" [x] Received '{routingKey}':'{message}'");
+};
+channel.BasicConsume(queue: queueName,
+    autoAck: true,
+    consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
+*/
+
+#endregion
+
+#region RPC
+
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "rpc_queue",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+var consumer = new EventingBasicConsumer(channel);
+channel.BasicConsume(queue: "rpc_queue",
+                     autoAck: false,
+                     consumer: consumer);
+Console.WriteLine(" [x] Awaiting RPC requests");
+
+consumer.Received += (model, ea) =>
+{
+    string response = string.Empty;
+
+    var body = ea.Body.ToArray();
+    var props = ea.BasicProperties;
+    var replyProps = channel.CreateBasicProperties();
+    replyProps.CorrelationId = props.CorrelationId;
+
+    try
+    {
+        var message = Encoding.UTF8.GetString(body);
+        int n = int.Parse(message);
+        Console.WriteLine($" [.] Fib({message})");
+        response = Fib(n).ToString();
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine($" [.] {e.Message}");
+        response = string.Empty;
+    }
+    finally
+    {
+        var responseBytes = Encoding.UTF8.GetBytes(response);
+        channel.BasicPublish(exchange: string.Empty,
+                             routingKey: props.ReplyTo,
+                             basicProperties: replyProps,
+                             body: responseBytes);
+        channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+    }
+};
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
+
+// Assumes only valid positive integer input.
+// Don't expect this one to work for big numbers, and it's probably the slowest recursive implementation possible.
+static int Fib(int n)
+{
+    if (n is 0 or 1)
+    {
+        return n;
+    }
+
+    return Fib(n - 1) + Fib(n - 2);
+}
+
+#endregion
---
+++ Send/Program.cs
@@ -0,0 +1,213 @@
+﻿#region a work queue is that each task is delivered to exactly one worker
+
+/*
+using System.Text;
+using RabbitMQ.Client;
+//https://www.rabbitmq.com/tutorials/tutorial-two-dotnet.html
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "task_queue",
+    durable: true,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+var message = GetMessage(args);
+var body = Encoding.UTF8.GetBytes(message);
+//if rabbit restart. Fro save message
+var properties = channel.CreateBasicProperties();
+properties.Persistent = true;
+
+channel.BasicPublish(exchange: string.Empty,
+    routingKey: "task_queue",
+    basicProperties: properties,
+    body: body);
+Console.WriteLine($" [x] Sent {message}");
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
+
+static string GetMessage(string[] args)
+{
+    return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+}
+*/
+
+#endregion
+
+#region exchange to Direct
+
+/*using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "logs_direct", type: ExchangeType.Direct);
+
+var message = GetMessage(args);
+var body = Encoding.UTF8.GetBytes(message);
+
+var strong = "logs";
+if (!message.Contains("Second") && !message.Contains("Fourth"))
+{
+    strong = "logs_direct";
+}
+
+
+channel.BasicPublish(exchange: strong,
+    routingKey: string.Empty,
+    basicProperties: null,
+    body: body);
+
+
+Console.WriteLine($" [x] Sent {message}");
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
+
+
+static string GetMessage(string[] args)
+{
+    return ((args.Length > 0) ? string.Join(" ", args) : "info: Hello World!");
+}*/
+
+#endregion
+
+#region ROUT_KYE
+
+/*
+using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "topic_logs", type: ExchangeType.Topic);
+
+var message = GetMessage(args);
+var body = Encoding.UTF8.GetBytes(message);
+
+//var strong = "topic_logs";
+/*if (!message.Contains("Second") && !message.Contains("Fourth"))
+{
+    strong = "topic_logs";
+}#1#
+
+channel.BasicPublish(exchange: "topic_logs",
+    routingKey: "ROUT_KYE",
+    basicProperties: null,
+    body: body);
+
+
+Console.WriteLine($" [x] Sent {message}");
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
+
+
+static string GetMessage(string[] args)
+{
+    return ((args.Length > 0) ? string.Join(" ", args) : "info: Hello World!");
+
+}
+*/
+
+
+#endregion
+
+#region RPC
+
+using System.Collections.Concurrent;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+public class RpcClient : IDisposable
+{
+    private const string QUEUE_NAME = "rpc_queue";
+
+    private readonly IConnection connection;
+    private readonly IModel channel;
+    private readonly string replyQueueName;
+    private readonly ConcurrentDictionary<string, TaskCompletionSource<string>> callbackMapper = new();
+
+    public RpcClient()
+    {
+        var factory = new ConnectionFactory { HostName = "localhost" };
+
+        connection = factory.CreateConnection();
+        channel = connection.CreateModel();
+        // declare a server-named queue
+        replyQueueName = channel.QueueDeclare().QueueName;
+        var consumer = new EventingBasicConsumer(channel);
+        consumer.Received += (model, ea) =>
+        {
+            if (!callbackMapper.TryRemove(ea.BasicProperties.CorrelationId, out var tcs))
+                return;
+            var body = ea.Body.ToArray();
+            var response = Encoding.UTF8.GetString(body);
+            tcs.TrySetResult(response);
+        };
+
+        channel.BasicConsume(consumer: consumer,
+                             queue: replyQueueName,
+                             autoAck: true);
+    }
+
+    public Task<string> CallAsync(string message, CancellationToken cancellationToken = default)
+    {
+        IBasicProperties props = channel.CreateBasicProperties();
+        var correlationId = Guid.NewGuid().ToString();
+        props.CorrelationId = correlationId;
+        props.ReplyTo = replyQueueName;
+        var messageBytes = Encoding.UTF8.GetBytes(message);
+        var tcs = new TaskCompletionSource<string>();
+        callbackMapper.TryAdd(correlationId, tcs);
+
+        channel.BasicPublish(exchange: string.Empty,
+                             routingKey: QUEUE_NAME,
+                             basicProperties: props,
+                             body: messageBytes);
+
+        cancellationToken.Register(() => callbackMapper.TryRemove(correlationId, out _));
+        return tcs.Task;
+    }
+
+    public void Dispose()
+    {
+        connection.Close();
+    }
+}
+
+public class Rpc
+{
+    public static async Task Main(string[] args)
+    {
+        Console.WriteLine("RPC Client");
+        string n = args.Length > 0 ? args[0] : "30";
+        await InvokeAsync(n);
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+
+    private static async Task InvokeAsync(string n)
+    {
+        using var rpcClient = new RpcClient();
+
+        Console.WriteLine(" [x] Requesting fib({0})", n);
+        var response = await rpcClient.CallAsync(n);
+        
+        Console.WriteLine(" [.] Got '{0}'", response);
+    }
+}
+
+#endregion
+
+
+
